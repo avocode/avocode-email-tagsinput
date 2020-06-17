f97f18c6274908d5e23e52ee21574ee01a2a7417
Ondřej Synáček <ondrejsynacek@fastmail.com>
2020-06-17 14:45:23 +0200

    use `avocode/avocode-email-tagsinput-babel-config` in test setup

--- test/jest.transform.js
+++ test/jest.transform.js
@@ -1,1 +1,1 @@
-module.exports = require('babel-jest').createTransformer(require('../babel.config.js'))
+module.exports = require('babel-jest').createTransformer(require('../packages/avocode-email-tagsinput-babel-config/lib/babel.config'))
